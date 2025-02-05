6bdf957b9ebd197990a1e42b30f8fbc8cebf6f56
Andrey <drjabaka@gmail.com>
2025-02-05 21:34:26 +0300

    prep for alembic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from fastapi import FastAPI
 import uvicorn
-from database.models.user import User  # noqa
-from database.models.order import Order  # noqa
 
 
 from core.api.user.router import router as user_router
